fa16714b0e39d72b3292f050b92fc42f542279de
turovtsov <dturovtsov@marathonbet.ru>
2019-05-08 13:21:43 +0300

    in progress...

--- www/js/page/home-page/c-home-page.js
+++ www/js/page/home-page/c-home-page.js
@@ -7,87 +7,20 @@
 import type {Node} from 'react';
 import React, {Component} from 'react';
 
-import type {ContextRouterType} from '../../type/react-router-dom-v4';
-
 import {Page} from '../../component/page/c-page';
-
 import {Header} from '../../component/header/c-header';
 import {Scroll} from '../../component/ui/scroll/c-scroll';
 import {CharacterList} from '../../component/character-list/c-character-list';
 
 import homePageStyle from './home-page.style.scss';
 
-type PropsType = ContextRouterType;
-
-class Mega<S, N: number> {
-    constructor(message: S) {
-        console.log(message);
-    }
-}
-
-export class HomePage extends Component<void, null> {
-    state: null;
-
-    componentDidMount() {
-
-        /*
-        const newModel = new MainModel<'prop', number>('prop', 1);
-
-        const onChangeProps = (oldValue: number | void, newValue: number | void) => {
-            console.log('newModel.attr');
-            console.log(oldValue, newValue);
-            console.log(newModel.attr);
-        };
-
-        newModel.onChange('prop', onChangeProps);
-
-        newModel.trigger('prop');
-        newModel.trigger('prop');
-        newModel.trigger('prop');
-
-        newModel.offChange('prop', onChangeProps);
-
-        // newModel.set({prop: 11});
-        newModel.set('prop', 12);
-        newModel.set('prop', 13);
-
-        console.log(newModel);
-
-        const mega = new Mega(1);
-
-        console.log(mega);
-
-        console.log(this.props.match);
-        */
-    }
-
-    props: PropsType;
-
-    handleMouseOver = () => {
-        const view = this;
-
-        console.log(view.state);
-    };
-
-    loadAsyncLoadTestComponent = async (): Promise<Node> => {
-        const {AsyncLoadTest} = await import(
-            /* webpackChunkName: 'async-load-test' */ '../../component/test/c-async-load-test'
-        );
-
-        return <AsyncLoadTest/>;
-    };
-
-    render(): Node {
-        const view = this;
-        const {props, state} = view;
-
-        return (
-            <Page>
-                <Header hasBackButton={false} hasMenuButton title="APP_NAME"/>
-                <Scroll>
-                    <CharacterList/>
-                </Scroll>
-            </Page>
-        );
-    }
+export function HomePage(): Node {
+    return (
+        <Page>
+            <Header hasBackButton={false} hasMenuButton title="APP_NAME"/>
+            <Scroll>
+                <CharacterList/>
+            </Scroll>
+        </Page>
+    );
 }
--- www/js/page/settings-page/c-settings-page.js
+++ www/js/page/settings-page/c-settings-page.js
@@ -34,12 +34,23 @@ export class SettingsPage extends Component<PropsType, StateType> {
     state: StateType;
     props: PropsType;
 
+    renderSelectPlatform(): [Node, Node] {
+        return [
+            <h3 className={settingsPageStyle.settings_page__part_header} key="header">
+                <Locale stringKey="SETTING__SELECT_PLATFORM"/>
+            </h3>,
+            <div key="content">content</div>,
+        ];
+    }
+
     render(): Node {
+        const view = this;
+
         return (
             <Page>
                 <Header hasBackButton hasMenuButton={false} title="SETTING__TITLE"/>
                 <Scroll>
-                    <Locale stringKey="SETTING__SELECT_PLATFORM"/>
+                    <div className={settingsPageStyle.settings_page__part}>{view.renderSelectPlatform()}</div>
                 </Scroll>
             </Page>
         );
