1241754d9b7a805090ff67ee75a53f155fafcdd3
turovtsov <dturovtsov@marathonbet.ru>
2019-05-21 19:15:35 +0300

    in progress...

--- www/js/character-data/helper.js
+++ www/js/character-data/helper.js
@@ -2,6 +2,15 @@
 
 import type {FrameDataType, MayBeNaType, MoveDataType, MoveType} from './character-type';
 
+/**
+ * Make move data
+ *
+ * @param {number} hitDamage - hit damage
+ * @param {number} blockDamage - block damage
+ * @param {number} flawlessBlockDamage - f/block damage
+ * @param {string} type - move type
+ * @returns {object} move data object
+ */
 export function getMoveData(
     hitDamage: MayBeNaType<number>,
     blockDamage: MayBeNaType<number>,
@@ -11,6 +20,18 @@ export function getMoveData(
     return {hitDamage, blockDamage, flawlessBlockDamage, type};
 }
 
+/**
+ * Make frame data
+ *
+ * @param {number} startUp - start up frames
+ * @param {number} active - active frames
+ * @param {number} recover - recover frames
+ * @param {number} cancel - cancel frames
+ * @param {number} hitAdvance - hit advance frames
+ * @param {number} blockAdvance - block advance frames
+ * @param {number} flawlessBlockAdvance - f/block advance frames
+ * @returns {object} frame data object
+ */
 // eslint-disable-next-line max-params
 export function getFrameData(
     startUp: MayBeNaType<number>,
