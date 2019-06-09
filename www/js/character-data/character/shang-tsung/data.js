3ed962a9f2a90de38a7ae046ba3a6054247a45a1
dim <web.best.master@gmail.com>
2019-06-09 14:26:18 +0300

    still in progress...

---
+++ www/js/character-data/character/shang-tsung/data.js
@@ -0,0 +1,28 @@
+// @flow
+
+/* eslint-disable sonarjs/no-duplicate-string, max-len */
+
+// WARNING: generated file!
+
+import faceImagePath from './face.png';
+
+export const shangTsung = {
+    id: 'shang-tsung',
+    imagePath: faceImagePath,
+    name: 'Shang Tsung',
+    move: {
+        basicList: [],
+        jumpingAttackList: [],
+        hopAttackList: [],
+        getUpAttackList: [],
+        flawlessBlockAttacksList: [],
+        throwsList: [],
+        rollEscapeList: [],
+        airEscapeList: [],
+        comboList: [],
+        specialList: [],
+        fatalBlowList: [],
+        fatalityList: [],
+        brutalityList: [],
+    },
+};
