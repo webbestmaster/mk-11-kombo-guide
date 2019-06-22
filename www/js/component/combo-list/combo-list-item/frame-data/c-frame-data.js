5d7ee3f2d428bd52b359b7d0266118b57bca16a8
turovtsov <dturovtsov@marathonbet.ru>
2019-06-22 17:29:56 +0300

    in progress...

--- www/js/component/combo-list/combo-list-item/frame-data/c-frame-data.js
+++ www/js/component/combo-list/combo-list-item/frame-data/c-frame-data.js
@@ -2,27 +2,42 @@
 
 /* eslint consistent-this: ["error", "view"] */
 
-import type {Node} from 'react';
+import type {ComponentType, Node} from 'react';
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import {Locale} from '../../../locale/c-locale';
 import type {ComboType} from '../../../../move-type/combo-type';
 import {moveTypeTranslationMap} from '../../../../move-type/move-type';
+import type {GlobalStateType} from '../../../../redux-store-provider/reducer';
+import type {SettingType} from '../../../../page/settings-page/reducer';
 
-import frameDataStyle from './frame-data.style.scss';
 import {renderFrameDataValue} from './helper';
+import frameDataStyle from './frame-data.style.scss';
+
+type ReduxPropsType = {|
+    +setting: SettingType,
+|};
+
+type ReduxActionType = {};
+
+const reduxAction: ReduxActionType = {};
 
 type PassedPropsType = {|
     +combo: ComboType,
 |};
 
-type PropsType = PassedPropsType;
+type PropsType = {
+    ...ReduxActionType,
+    ...PassedPropsType,
+    ...ReduxPropsType,
+};
 
 type StateType = {|
     +state: number,
 |};
 
-export class FrameData extends Component<PropsType, StateType> {
+class FrameData extends Component<PropsType, StateType> {
     constructor(props: PropsType) {
         super(props);
 
@@ -35,6 +50,14 @@ export class FrameData extends Component<PropsType, StateType> {
 
     state: StateType;
     props: PropsType;
+
+    renderFlawlessColumn(node: Node): Node {
+        const view = this;
+        const {props} = view;
+        const {setting} = props;
+
+        return setting.isShowFlawlessData ? node : null;
+    }
 
     renderMoveData(): Node {
         const view = this;
@@ -55,9 +78,11 @@ export class FrameData extends Component<PropsType, StateType> {
                     <td className={frameDataStyle.frame_data__table_th}>
                         <Locale stringKey="FRAME_DATA__BLOCK_DAMAGE"/>
                     </td>
-                    <td className={frameDataStyle.frame_data__table_th}>
-                        <Locale stringKey="FRAME_DATA__FLAWLESS_BLOCK_DAMAGE"/>
-                    </td>
+                    {view.renderFlawlessColumn(
+                        <td className={frameDataStyle.frame_data__table_th}>
+                            <Locale stringKey="FRAME_DATA__FLAWLESS_BLOCK_DAMAGE"/>
+                        </td>
+                    )}
                 </tr>
                 <tr className={frameDataStyle.frame_data__table_tr}>
                     <td className={frameDataStyle.frame_data__table_td}>
@@ -65,7 +90,11 @@ export class FrameData extends Component<PropsType, StateType> {
                     </td>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(hitDamage)}</td>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(blockDamage)}</td>
-                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(flawlessBlockDamage)}</td>
+                    {view.renderFlawlessColumn(
+                        <td className={frameDataStyle.frame_data__table_td}>
+                            {renderFrameDataValue(flawlessBlockDamage)}
+                        </td>
+                    )}
                 </tr>
             </>
         );
@@ -90,13 +119,13 @@ export class FrameData extends Component<PropsType, StateType> {
                     <td className={frameDataStyle.frame_data__table_th}>
                         <Locale stringKey="FRAME_DATA__RECOVERY"/>
                     </td>
-                    <td className={frameDataStyle.frame_data__table_th}>&nbsp;</td>
+                    {view.renderFlawlessColumn(<td className={frameDataStyle.frame_data__table_th}>&nbsp;</td>)}
                 </tr>
                 <tr className={frameDataStyle.frame_data__table_tr}>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(startUp)}</td>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(active)}</td>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(recover)}</td>
-                    <td className={frameDataStyle.frame_data__table_td}>&nbsp;</td>
+                    {view.renderFlawlessColumn(<td className={frameDataStyle.frame_data__table_td}>&nbsp;</td>)}
                 </tr>
                 <tr className={frameDataStyle.frame_data__table_tr}>
                     <td className={frameDataStyle.frame_data__table_th}>
@@ -108,17 +137,21 @@ export class FrameData extends Component<PropsType, StateType> {
                     <td className={frameDataStyle.frame_data__table_th}>
                         <Locale stringKey="FRAME_DATA__CANCEL_ADVANTAGE"/>
                     </td>
-                    <td className={frameDataStyle.frame_data__table_th}>
-                        <Locale stringKey="FRAME_DATA__FLAWLESS_BLOCK_ADVANTAGE"/>
-                    </td>
+                    {view.renderFlawlessColumn(
+                        <td className={frameDataStyle.frame_data__table_th}>
+                            <Locale stringKey="FRAME_DATA__FLAWLESS_BLOCK_ADVANTAGE"/>
+                        </td>
+                    )}
                 </tr>
                 <tr className={frameDataStyle.frame_data__table_tr}>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(hitAdvance)}</td>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(blockAdvance)}</td>
                     <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(cancel)}</td>
-                    <td className={frameDataStyle.frame_data__table_td}>
-                        {renderFrameDataValue(flawlessBlockAdvance)}
-                    </td>
+                    {view.renderFlawlessColumn(
+                        <td className={frameDataStyle.frame_data__table_td}>
+                            {renderFrameDataValue(flawlessBlockAdvance)}
+                        </td>
+                    )}
                 </tr>
             </>
         );
@@ -141,3 +174,12 @@ export class FrameData extends Component<PropsType, StateType> {
         );
     }
 }
+
+const ConnectedComponent = connect<ComponentType<FrameData>, PassedPropsType, ReduxPropsType, ReduxActionType>(
+    (state: GlobalStateType, props: PassedPropsType): ReduxPropsType => ({
+        setting: state.setting,
+    }),
+    reduxAction
+)(FrameData);
+
+export {ConnectedComponent as FrameData};
