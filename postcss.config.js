38d7e04c89ffb135f35cde23c6790bf821c51fc0
turovtsov <dturovtsov@marathonbet.ru>
2019-07-01 10:49:06 +0300

    in progress...

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = ({file, options, environment}) => ({
     // eslint-disable-line no-unused-vars
-    parser: false,
+    // parser: false,
     // parser: file.extname === '.sss' ? 'sugarss' : false,
-    browserslist: ['Chrome 20', 'Safari 5', 'Edge 12', 'Explorer 8', 'Firefox 15'],
+    // browserslist: ['Chrome 20', 'Safari 5', 'Edge 12', 'Explorer 8', 'Firefox 15'],
+    plugins: [
+        autoprefixer({
+            overrideBrowserslist: ['last 1 version', '> 1%', 'IE 10'],
+        }),
+    ],
 });
