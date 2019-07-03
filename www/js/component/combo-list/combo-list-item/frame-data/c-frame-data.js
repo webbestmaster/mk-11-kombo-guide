253d852718eaddca647818f7b201a958194b34df
turovtsov <dturovtsov@marathonbet.ru>
2019-07-03 14:25:22 +0300

    in progress...

--- www/js/component/combo-list/combo-list-item/frame-data/c-frame-data.js
+++ www/js/component/combo-list/combo-list-item/frame-data/c-frame-data.js
@@ -5,12 +5,14 @@
 import type {ComponentType, Node} from 'react';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import classNames from 'classnames';
 
 import {Locale} from '../../../locale/c-locale';
 import type {ComboType} from '../../../../move-type/combo-type';
 import {moveTypeTranslationMap} from '../../../../move-type/move-type';
 import type {GlobalStateType} from '../../../../redux-store-provider/reducer';
 import type {SettingType} from '../../../../page/settings-page/reducer';
+import {frameDataViewTypeMap} from '../../../../page/settings-page/redux/frame-data-view-type/action';
 
 import {renderFrameDataValue} from './helper';
 import frameDataStyle from './frame-data.style.scss';
@@ -59,7 +61,7 @@ class FrameData extends Component<PropsType, StateType> {
         return setting.isShowFlawlessData ? node : null;
     }
 
-    renderMoveData(): Node {
+    renderMoveDataCompact(): Node {
         const view = this;
         const {props} = view;
         const {combo} = props;
@@ -100,7 +102,7 @@ class FrameData extends Component<PropsType, StateType> {
         );
     }
 
-    renderFrameData(): Node {
+    renderFrameDataCompact(): Node {
         const view = this;
         const {props} = view;
         const {combo} = props;
@@ -157,21 +159,138 @@ class FrameData extends Component<PropsType, StateType> {
         );
     }
 
-    render(): Node {
+    renderMoveDataLikeInGame(): Node {
         const view = this;
         const {props} = view;
         const {combo} = props;
+        const {moveData} = combo;
+        const {type, hitDamage, blockDamage, flawlessBlockDamage} = moveData;
+
+        return (
+            <>
+                <tr className={frameDataStyle.frame_data__table_tr}>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__HIT_DAMAGE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__BLOCK_DAMAGE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__FLAWLESS_BLOCK_DAMAGE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__MOVE_TYPE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th} colSpan="3">
+                        &nbsp;
+                    </td>
+                </tr>
+                <tr className={frameDataStyle.frame_data__table_tr}>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(hitDamage)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(blockDamage)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(flawlessBlockDamage)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>
+                        <Locale stringKey={moveTypeTranslationMap[type]}/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_td} colSpan="3">
+                        &nbsp;
+                    </td>
+                </tr>
+            </>
+        );
+    }
+
+    renderFrameDataLikeInGame(): Node {
+        const view = this;
+        const {props} = view;
+        const {combo} = props;
+        const {frameData} = combo;
+        const {startUp, active, recover, hitAdvance, blockAdvance, cancel, flawlessBlockAdvance} = frameData;
+
+        return (
+            <>
+                <tr className={frameDataStyle.frame_data__table_tr}>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__START_UP"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__ACTIVE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__RECOVERY"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__CANCEL_ADVANTAGE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__HIT_ADVANTAGE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__BLOCK_ADVANTAGE"/>
+                    </td>
+                    <td className={frameDataStyle.frame_data__table_th}>
+                        <Locale stringKey="FRAME_DATA__FLAWLESS_BLOCK_ADVANTAGE"/>
+                    </td>
+                </tr>
+                <tr className={frameDataStyle.frame_data__table_tr}>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(startUp)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(active)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(recover)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(cancel)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(hitAdvance)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>{renderFrameDataValue(blockAdvance)}</td>
+                    <td className={frameDataStyle.frame_data__table_td}>
+                        {renderFrameDataValue(flawlessBlockAdvance)}
+                    </td>
+                </tr>
+            </>
+        );
+    }
+
+    renderTableWrapper(children: Node | Array<Node>): Node {
+        const view = this;
+        const {props} = view;
+        const {combo, setting} = props;
+        const {frameDataViewType} = setting;
+
+        const tableClassName = classNames({
+            [frameDataStyle.frame_data__table__compact]: frameDataViewType === frameDataViewTypeMap.compact,
+            [frameDataStyle.frame_data__table__like_in_game]: frameDataViewType === frameDataViewTypeMap.likeInGame,
+        });
 
         return (
             <div className={frameDataStyle.frame_data__wrapper}>
-                <table className={frameDataStyle.frame_data__table}>
-                    <tbody className={frameDataStyle.frame_data__table_body}>
-                        {view.renderMoveData()}
-                        {view.renderFrameData()}
-                    </tbody>
+                <table className={tableClassName}>
+                    <tbody className={frameDataStyle.frame_data__table_body}>{children}</tbody>
                 </table>
             </div>
         );
+    }
+
+    render(): Node {
+        const view = this;
+        const {props} = view;
+        const {combo, setting} = props;
+
+        if (setting.frameDataViewType === frameDataViewTypeMap.compact) {
+            return view.renderTableWrapper(
+                <>
+                    {view.renderMoveDataCompact()}
+                    {view.renderFrameDataCompact()}
+                </>
+            );
+        }
+
+        if (setting.frameDataViewType === frameDataViewTypeMap.likeInGame) {
+            return view.renderTableWrapper(
+                <>
+                    {view.renderMoveDataLikeInGame()}
+                    {view.renderFrameDataLikeInGame()}
+                </>
+            );
+        }
+
+        throw new Error('can not detect frameDataViewType');
     }
 }
 
