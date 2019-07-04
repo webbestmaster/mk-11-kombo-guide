354f02c3d43d02b7c58c120479f4cc84790f37c9
turovtsov <dturovtsov@marathonbet.ru>
2019-07-04 20:21:48 +0300

    in progress...

--- util/get-data-from-game/image.js
+++ util/get-data-from-game/image.js
@@ -3,8 +3,29 @@
 import getPixelsFromNpm from 'get-pixels';
 import type {ImageDataType} from 'get-pixels';
 
-function getPixels(pathToImage: string): Promise<ImageDataType> {
-    return new Promise((resolve: (imageData: ImageDataType) => mixed, reject: (error: Error) => mixed) => {
+export type CoordinatesType = {|
+    +x: number,
+    +y: number,
+|};
+
+export type PixelType = {|
+    +x: number,
+    +y: number,
+    +red: number,
+    +green: number,
+    +blue: number,
+    +alpha: number,
+|};
+
+type FullImageDataType = {|
+    +width: number,
+    +height: number,
+    +numberList: Array<number>,
+    +pixelList: Array<PixelType>,
+|};
+
+export function getImageData(pathToImage: string): Promise<FullImageDataType> {
+    return new Promise((resolve: (imageData: FullImageDataType) => mixed, reject: (error: Error) => mixed) => {
         getPixelsFromNpm(pathToImage, (mayBeError: Error | null, mayBeImageData: ImageDataType | void) => {
             if (mayBeError instanceof Error) {
                 reject(mayBeError);
@@ -12,9 +33,12 @@ function getPixels(pathToImage: string): Promise<ImageDataType> {
             }
 
             if (mayBeImageData) {
-                const {data, shape, stride, offset} = mayBeImageData;
+                const {data, shape} = mayBeImageData;
+                const [width, height] = shape;
+                const numberList = [...data];
+                const pixelList = makePixelArray({numberList, width, height, pixelList: []});
 
-                resolve({data: [...data], shape, stride, offset});
+                resolve({numberList, width, height, pixelList});
                 return;
             }
 
@@ -23,4 +47,33 @@ function getPixels(pathToImage: string): Promise<ImageDataType> {
     });
 }
 
-getPixels('./_res/small-image.png');
+// getImageData('./_res/screenshot/1.png').then(console.log)
+// getImageData('./_res/small-image.png');
+
+function makePixelArray(fullImageDataType: FullImageDataType): Array<PixelType> {
+    const result = [];
+
+    const {width, height, numberList} = fullImageDataType;
+
+    // eslint-disable-next-line no-loops/no-loops
+    for (let heightIndex = 0; heightIndex < height; heightIndex += 1) {
+        const line = numberList.slice(heightIndex * width * 4, (heightIndex + 1) * width * 4);
+
+        // eslint-disable-next-line no-loops/no-loops
+        for (let widthIndex = 0; widthIndex < width; widthIndex += 1) {
+            const [red, green, blue, alpha] = line.slice(widthIndex * 4, (widthIndex + 1) * 4);
+
+            result.push({x: widthIndex, y: heightIndex, red, green, blue, alpha});
+        }
+    }
+
+    return result;
+}
+
+/*
+export async function getSubImageCoordinates(pathToBigImage: string, pathToSmallImage: string): CoordinatesType | null {
+    const bigImageData = await getImageData(pathToBigImage);
+    const smallImageData = await getImageData(pathToSmallImage);
+
+}
+*/
