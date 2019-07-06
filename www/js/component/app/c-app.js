83f7cf6664aea0efc3c73bc0dbdd0515919732b4
turovtsov <dturovtsov@marathonbet.ru>
2019-07-06 13:34:13 +0300

    in progress...

--- www/js/component/app/c-app.js
+++ www/js/component/app/c-app.js
@@ -2,8 +2,10 @@
 
 /* global PUBLIC_PATH */
 
+/* eslint consistent-this: ["error", "view"] */
+
 import type {Node} from 'react';
-import React from 'react';
+import React, {Component} from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 
 import {ReduxStoreProvider} from '../../redux-store-provider/provider';
@@ -16,6 +18,7 @@ import {LoginPage} from '../../page/login-page/c-login-page';
 import {PageNotFound} from '../../page/page-not-found/c-page-not-found';
 import {SettingsPage} from '../../page/settings-page/c-settings-page';
 import {CharacterPage} from '../../page/character-page/c-character-page';
+import {forceResize} from '../../lib/screen';
 
 import {routes} from './routes';
 
@@ -24,28 +27,37 @@ console.log(appConst);
 // eslint-disable-next-line id-match
 const publicPath = PUBLIC_PATH;
 
-export function App(): Node {
-    return (
-        /* eslint-disable react/jsx-max-depth */
-        // you can replace the extra <div> with any react component
-        <ReduxStoreProvider>
-            <>
-                <Auth key="auth"/>
-                <System key="system">
-                    <HashRouter basename={publicPath}>
-                        <Switch key="switch">
-                            <Route component={userIsNotAuthenticated(LoginPage)} exact path={routes.login}/>
+export class App extends Component {
+    static renderRouter(): Node {
+        return (
+            <HashRouter basename={publicPath}>
+                <Switch key="switch">
+                    <Route component={userIsNotAuthenticated(LoginPage)} exact path={routes.login}/>
 
-                            <Route component={userIsAuthenticated(HomePage)} exact path={routes.index}/>
-                            <Route component={userIsAuthenticated(SettingsPage)} exact path={routes.settings}/>
-                            <Route component={userIsAuthenticated(CharacterPage)} exact path={routes.character}/>
+                    <Route component={userIsAuthenticated(HomePage)} exact path={routes.index}/>
+                    <Route component={userIsAuthenticated(SettingsPage)} exact path={routes.settings}/>
+                    <Route component={userIsAuthenticated(CharacterPage)} exact path={routes.character}/>
 
-                            <Route component={PageNotFound}/>
-                        </Switch>
-                    </HashRouter>
-                </System>
-            </>
-        </ReduxStoreProvider>
-        /* eslint-enable react/jsx-max-depth */
-    );
+                    <Route component={PageNotFound}/>
+                </Switch>
+            </HashRouter>
+        );
+    }
+
+    async componentDidMount() {
+        await forceResize();
+
+        console.log('---> App: forceResize() is done');
+    }
+
+    render(): Node {
+        return (
+            <ReduxStoreProvider>
+                <>
+                    <Auth key="auth"/>
+                    <System key="system">{App.renderRouter()}</System>
+                </>
+            </ReduxStoreProvider>
+        );
+    }
 }
