6d263050bda8748ff83c1322c1c2c29ce921eaa8
turovtsov <dturovtsov@marathonbet.ru>
2019-07-06 13:43:55 +0300

    in progress...

--- webpack.config.js
+++ webpack.config.js
@@ -48,14 +48,14 @@ const fileRegExp = /\.(png|jpg|jpeg|gif|svg|otf|ttf|woff2?)$/;
 
 const pathToDist = '/dist';
 
-const duplicateList = ['/@babel/runtime', 'warning', 'invariant', 'hoist-non-react-statics'];
+const duplicateList = ['regenerator-runtime', '/@babel/runtime', 'warning', 'invariant', 'hoist-non-react-statics'];
 
 const alias = duplicateList.reduce((accumulator, packageName) => {
     return {...accumulator, [packageName]: path.resolve(CWD, `node_modules/${packageName}`)};
 }, {});
 
 const webpackConfig = {
-    entry: ['swiper/dist/css/swiper.css', './www/css/root.scss', './www/js/root.js'],
+    entry: ['babel-polyfill', 'whatwg-fetch', 'swiper/dist/css/swiper.css', './www/css/root.scss', './www/js/root.js'],
     output: {
         path: path.join(CWD, pathToDist),
         publicPath,
